5ff045b75b5380758caa898ece0a1927f269dca2
Phil D <pcdrummond@icloud.com>
2025-01-13 10:57:36 -0800

    Configures taskflow.

--- src/pages/privacy-timeline/_config/taskflow.config.ts
+++ src/pages/privacy-timeline/_config/taskflow.config.ts
@@ -1,111 +1,43 @@
-import { CompareDataConfig } from "./taskflow.types";
+import { transformCsvData } from '../utils/transformData';
 
-export const taskflow: CompareDataConfig = {
-  properties: {
-    /**
-     * Word (in singular form) to use in the UI for the item being compared. 
-     * For example, if the app is comparing plants, this value should be "plant".
-     */
-    itemName: "scenario",
-    /**
-     * Pluralized version of itemName. This is used in the UI when itemName needs to be plural. 
-     * For example, if the app is comparing plants, this value should be "plants".
-     */
-    itemNamePlural: "scenarios",
-  },
-  data: {
-    items: {
-      /**
-       * Source of the data for the initial list of items. 
-       */
-      source: "data/default/compare-data/scenarios.json",
-      /**
-       * Field name for the unique ID in the data source.
-       */
-      idField: "id"
-    }
-  },
-  pages: {
-    index: {
-      /**
-       * Title to appear at the top of the initial items list page.
-       */
-      title: "Compare Data App",
-      /**
-       * Text to appear underneath the title at the top of the initial items list page.
-       */
-      description: "Description of this app section",
-      /**
-       * Column definitions for the table on the initial page.
-       * See all options from MUI's DataGrid: https://mui.com/x/api/data-grid/grid-col-def/
-       */
-      tableColumns: [
-        {
-          field: "name",
-          headerName: "Scenario Name",
-          width: 200
-        },
-        {
-          field: "description",
-          headerName: "Description",
-          width: 200
-        },
-        {
-          field: "analysis_type",
-          headerName: "Analysis Type",
-          width: 200
-        },
-        {
-          field: "volumetric_flow_rate",
-          headerName: "Volumetric Flow Rate",
-          width: 200,
-          isComparisonMetric: true
-        },
-        {
-          field: "tss_concentration",
-          headerName: "TSS Concentration",
-          width: 200,
-          isComparisonMetric: true
-        },
-        {
-          field: "cod_concentration",
-          headerName: "COD Concentration",
-          width: 200,
-          isComparisonMetric: true
-        },
-        {
-          field: "tkn_concentration",
-          headerName: "TKN Concentration",
-          width: 200,
-          isComparisonMetric: true
-        },
-        {
-          field: "acetic_acid_concentration",
-          headerName: "Acetic Acid Concentration",
-          width: 200,
-          isComparisonMetric: true
-        }
-      ]
+export const config = {
+  taskType: 'TimelineViz',
+  dataConfig: {
+    source: {
+      type: 'csv',
+      path: '/data/privacy_laws.csv',
+      transformer: transformCsvData,
     },
-    new: {
-      /**
-       * Title to appear at the top of the new item page.
-       */
-      title: "New Scenario",
-      /**
-       * Text to appear underneath the title at the top of the new item page.
-       */
-      description: "Description of this app section"
+  },
+  visualConfig: {
+    timeline: {
+      height: 800,
+      margin: { top: 40, right: 120, bottom: 60, left: 200 },
     },
-    compare: {
-      /**
-       * Title to appear at the top of the comparison page.
-       */
-      title: "Compare Scenarios",
-      /**
-       * Text to appear underneath the title at the top of the comparison page.
-       */
-      description: "Description of this app section"
-    }
-  }
-}
+    grouping: {
+      field: 'jurisdictionName',
+      sortBy: 'promulgationDate',
+    },
+    markers: {
+      types: [
+        {
+          field: 'promulgationDate',
+          symbol: 'circle',
+          size: 8,
+        },
+        {
+          field: 'latestRevision',
+          symbol: 'diamond',
+          size: 10,
+        },
+      ],
+    },
+    colors: {
+      status: {
+        'In Effect': '#2ecc71',
+        Repealed: '#e74c3c',
+        NYIF: '#f1c40f',
+      },
+    },
+  },
+};
